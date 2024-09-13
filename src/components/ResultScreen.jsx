abf4799d722c16e1e0eaf35193fd487f5f9b2bc6
Ahmad <ahmad.mahmoud9523@gmail.com>
2024-09-13 18:32:06 -0400

    Initial commit with project files2

--- src/components/ResultScreen.jsx
+++ src/components/ResultScreen.jsx
@@ -4,7 +4,7 @@ function ResultScreen(){
     return (
         <div className="h-[512px] w-[368px] bg-gradient-light-slate-blue rounded-3xl flex flex-col justify-start items-center">
             <h1 className="text-custom-light-blue text-2xl pt-[38px] font-hanken font-medium">
-                Your Result
+                Your Result 
                 </h1>
             <div className="h-[200px] w-[200px] rounded-full flex flex-col justify-center items-center bg-circle-gradient mt-[28px]">
                 <h1 className="font-hanken font-extrabold text-white text-[72px] ">76</h1>
