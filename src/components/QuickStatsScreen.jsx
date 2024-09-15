d02ee6a7d16b5c6073087416f5cc9325fa8bdebb
Ahmad <ahmad.mahmoud9523@gmail.com>
2024-09-14 21:56:18 -0400

    final commit with github pages edit fully working

--- src/components/QuickStatsScreen.jsx
+++ src/components/QuickStatsScreen.jsx
@@ -12,26 +12,34 @@ function QuickStatsScreen(){
 
     const dataArray = quickData.map(item => {
         let icon;
+        let hoverClass;
         switch (item.category) {
           case 'Reaction':
             icon = iconReaction;
+            hoverClass = 'hover:bg-rose-200';
             break;
           case 'Memory':
             icon = iconMemory;
+            hoverClass = 'hover:bg-amber-200';
             break;
           case 'Verbal':
             icon = iconVerbal;
+            hoverClass = 'hover:bg-green-200';
             break;
           case 'Visual':
             icon = iconVisual;
+            hoverClass = 'hover:bg-indigo-200';
             break;
           default:
-            icon = ''; // Default icon or leave it blank
+            icon = '';
+            hoverClass = '';
         }
     
-        // Return the modified object with the correct icon
-        return { ...item, icon };
+        return { ...item, icon, hoverClass };
       });
+    
+
+      console.log(dataArray)
    
 
 
@@ -41,7 +49,7 @@ function QuickStatsScreen(){
         
       
 
-        <div key={index} className={`flex space-x-2 ${item.boxColor} hover:${item.hoverColor} rounded-md w-[288px] h-[56px] items-center px-[16px]`}>
+        <div key={index} className={`flex space-x-2 ${item.boxColor} ${item.hoverColor} rounded-md w-[288px] h-[56px] items-center px-[16px]`}>
             <img src={item.icon} />
             <h1 className={`flex-1 font-hanken font-medium text-[18px] ${item.textColor}`}>{item.category}</h1>
             <h1 className={`text-[18px]`}><strong>{item.score}</strong>  /  100</h1>
